43afbe81c64c8588f4fc04117c9b4ebaa0cdfb7b
Tyler_Han <tylerhan1226@gmail.com>
2023-12-20 15:59:02 -0800

    phase-1

--- backend/app.js
+++ backend/app.js
@@ -45,4 +45,15 @@ const routes = require('./routes/index');
 
 app.use(routes); // Connect all the routes
 
+// backend/app.js
+// ...
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
 module.exports = app;
