76946ba58ab084513b01003e9bbbd122c89b2179
Tyler_Han <tylerhan1226@gmail.com>
2023-12-20 18:24:41 -0800

    phase-1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
@@ -14,5 +21,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+
 
 module.exports = router;
