8671f5320038cf788fec2130b9c5f7188fd9995f
Tyler_Han <tylerhan1226@gmail.com>
2023-12-21 09:47:54 -0800

    phase-4-01

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,18 +4,16 @@ const router = require('express').Router();
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
-// backend/routes/api/index.js
-// ...
-
-// backend/routes/api/index.js
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
-
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
@@ -34,9 +32,6 @@ router.get('/set-token-cookie', async (_req, res) => {
 
 // backend/routes/api/index.js
 // ...
-
-
-
 router.get(
   '/restore-user',
   (req, res) => {
