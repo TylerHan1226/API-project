bde65a2e22eca3f5bd96ab721faf6dc048b360e6
Tyler_Han <tylerhan1226@gmail.com>
2023-12-21 13:33:27 -0800

    deploy4

--- backend/db/seeders/20231221171553-demo-user.js
+++ backend/db/seeders/20231221171553-demo-user.js
@@ -15,18 +15,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Tyler',
+        lastName: 'Han',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        
+        firstName: 'Kevin',
+        lastName: 'Fan',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        
+        firstName: 'Dennis',
+        lastName: 'Ma',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
