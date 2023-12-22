1261637b109858db902e6644d56db7819efb8b9a
Tyler_Han <tylerhan1226@gmail.com>
2023-12-22 13:49:36 -0800

    dev merge to main .1

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.Group, {
-        foreignKey: 'organizerId'
+        foreignKey: 'id'
       })
     }
   };
