5c90aebb5795af83eb18d7abef8ee8c6eebfed3e
Tyler_Han <tylerhan1226@gmail.com>
2024-01-05 15:22:29 -0800

    updated user seeder

--- backend/db/seeders/20231221171553-demo-user.js
+++ backend/db/seeders/20231221171553-demo-user.js
@@ -53,6 +53,48 @@ module.exports = {
         firstName: 'Chris',
         lastName: 'Lin',
         hashedPassword: bcrypt.hashSync('password6')
+      },
+      {
+        email: 'user7@user.io',
+        username: 'User7',
+        firstName: 'Jack',
+        lastName: 'Nicholson',
+        hashedPassword: bcrypt.hashSync('password7')
+      },
+      {
+        email: 'user8@user.io',
+        username: 'User8',
+        firstName: 'Edward',
+        lastName: 'Norton',
+        hashedPassword: bcrypt.hashSync('password8')
+      },
+      {
+        email: 'user9@user.io',
+        username: 'User9',
+        firstName: 'Christian',
+        lastName: 'Bale',
+        hashedPassword: bcrypt.hashSync('password9')
+      },
+      {
+        email: 'user10@user.io',
+        username: 'User10',
+        firstName: 'Erling',
+        lastName: 'Haaland',
+        hashedPassword: bcrypt.hashSync('password10')
+      },
+      {
+        email: 'user11@user.io',
+        username: 'User11',
+        firstName: 'Marilyn',
+        lastName: 'Manson',
+        hashedPassword: bcrypt.hashSync('password11')
+      },
+      {
+        email: 'user12@user.io',
+        username: 'User12',
+        firstName: 'Jessica',
+        lastName: 'Audiffred',
+        hashedPassword: bcrypt.hashSync('password12')
       }
     ], { validate: true });
   },
--- backend/db/seeders/20231223002736-groups.js
+++ backend/db/seeders/20231223002736-groups.js
@@ -14,8 +14,8 @@ module.exports = {
    await Group.bulkCreate([
     {
       organizerId: 1,
-      name: 'EDM squad',
-      about: 'This is a group for everyone loves EDM!',
+      name: 'EDM Squad 🎶',
+      about: "🎶 Welcome to EDM Squad, the ultimate gathering for electronic music enthusiasts in LA! Immerse yourself in the pulsating beats of EDM, with a special focus on the electrifying world of EDM. Our mission is to unite like-minded individuals who share a passion for bass drops, mind-bending synths, and the unparalleled energy of live EDM events. Let the bass guide you, and let's create epic memories together! 🎶",
       type: 'In person',
       private: false,
       city: 'Los Angeles',
@@ -23,8 +23,8 @@ module.exports = {
     },
     {
       organizerId: 2,
-      name: 'League of Legends Team',
-      about: 'The best team of 5',
+      name: 'The Best 5 🎮',
+      about: "🎮 Welcome to 'The Best 5' - your gateway to exhilarating League of Legends competition! Dive into weekend showdowns where strategy meets skill on the Summoner's Rift. Whether you're a seasoned player or a rising star, join our community for epic battles, unforgettable plays, and the pursuit of victory. Unleash your gaming passion with us! 🎮",
       type: 'Online',
       private: false,
       city: 'Cincinnati',
@@ -32,8 +32,8 @@ module.exports = {
     },
     {
       organizerId: 3,
-      name: 'Weekend hoopers',
-      about: 'The weekend gathering basketball group',
+      name: "NYC Ballers' Expedition 🏀",
+      about: "🏀 Welcome to 'NYC Ballers' Expedition' - where the heartbeat of New York City meets the rhythm of the basketball court. Join us on weekends as we navigate the city's concrete jungle in search of the best hoops spots. Whether you're a street ball aficionado or a casual player, our group is your ticket to thrilling games, breathtaking views, and a tight-knit community of fellow NYC ballers. Lace up your sneakers, bring your A-game, and let's dribble through the iconic streets of NYC together. Every weekend is a new slam dunk waiting to happen! 🏀",
       type: 'In person',
       private: false,
       city: 'New York City',
