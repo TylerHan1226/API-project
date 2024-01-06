a60e6f76b01b7b251f9ddfad36cd1de6ca59744a
Tyler_Han <tylerhan1226@gmail.com>
2024-01-05 16:04:43 -0800

    updated group about(reduce length)

--- backend/db/seeders/20231223002736-groups.js
+++ backend/db/seeders/20231223002736-groups.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       organizerId: 1,
       name: 'EDM Squad',
-      about: "Welcome to EDM Squad, the ultimate gathering for electronic music enthusiasts in LA! Immerse yourself in the pulsating beats of EDM, with a special focus on the electrifying world of EDM. Let the bass guide you, and let's create epic memories together!",
+      about: "'Welcome to EDM Squad'",
       type: 'In person',
       private: false,
       city: 'Los Angeles',
@@ -24,7 +24,7 @@ module.exports = {
     {
       organizerId: 2,
       name: 'The Best 5',
-      about: "Welcome to 'The Best 5' - your gateway to exhilarating League of Legends competition! Dive into weekend showdowns where strategy meets skill on the Summoner's Rift. Unleash your gaming passion with us! 🎮",
+      about: "Welcome to 'The Best 5'",
       type: 'Online',
       private: false,
       city: 'Cincinnati',
@@ -33,7 +33,7 @@ module.exports = {
     {
       organizerId: 3,
       name: "NYC Ballers' Expedition",
-      about: "Welcome to 'NYC Ballers' Expedition' - where the heartbeat of New York City meets the rhythm of the basketball court. Join us on weekends as we navigate the city's concrete jungle in search of the best hoops spots. Every weekend is a new slam dunk waiting to happen!",
+      about: "Welcome to 'NYC Ballers' Expedition'",
       type: 'In person',
       private: false,
       city: 'New York City',
